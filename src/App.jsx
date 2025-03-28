635ff57b40ec713377e7c51e7d79a186a6a43970
Ashley Laisure <ashley.laisure@gmail.com>
2025-03-27 20:59:37 -0400

    thurday class

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,67 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import * as petService from './services/petService.js'
+import PetList from './components/PetList/PetList.jsx';
+import PetDetail from './components/PetDetail/PetDetail.jsx';
+import PetForm from './components/PetForm/PetForm.jsx'
 import './App.css'
 
 function App() {
 
+  const [pets, setPets] = useState([]);
+  const [selected, setSelected] = useState(null);
+  const [isFormOpen, setIsFormOpen] = useState(false);
+
+  const handleSelect = (pet) => {
+    setSelected(pet)
+  }
+
+  const handleFormView = () => {
+    setIsFormOpen(!isFormOpen)
+  }
+
+  const handleAddPet = async (formData) => {
+    try {
+      const newPet = await petService.create(formData);
+      console.log(newPet)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+    const fetchPets = async () => {
+
+      try {
+        const fetchedPets = await petService.index()
+        if (fetchedPets.err) {
+          throw new Error(fetchedPets.err)
+        }
+        setPets(fetchedPets)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+
+    fetchPets()
+  }, []);
+
+  console.log("pets:", pets)
 
   return (
     <>
-      <h1>Hello World!</h1>
+      <PetList 
+        pets={pets} 
+        handleSelect={handleSelect}
+        handleFormView={handleFormView}
+        isFormOpen={isFormOpen}/>
+
+      {/* {isFormOpen && <PetForm />} */}
+
+      {isFormOpen ? (
+        <PetForm handleAddPet={handleAddPet}/>
+        ) : (
+        <PetDetail selected={selected}/>)}
+
     </>
   )
 }
---
+++ src/services/petService.js
@@ -0,0 +1,29 @@
+// src/services/petService.js
+
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/pets`;
+
+const index = async () => {
+    try {
+        const res = await fetch(BASE_URL)
+        return res.json();
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+const create = async (formData) => {
+    try {
+        const res = await fetch(BASE_URL, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json',},
+            body: JSON.stringify(formData),
+        });
+        return res.json();
+        
+    } catch (err) {
+        console.log(err)
+    }
+}
+console.log(await index());
+
+export {index, create}
